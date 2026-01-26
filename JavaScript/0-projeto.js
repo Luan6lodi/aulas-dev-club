a05b4da3c492e14a7f67806a57e17f16821a9799
luan6lodi <luan6loid@gmail.com>
2026-01-26 16:44:22 -0300

    aula dia 26/01/26

--- JavaScript/0-projeto.js
+++ JavaScript/0-projeto.js
@@ -3,11 +3,25 @@ const convertButton = document.querySelector(".convert-button");
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
 
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert",
+  ); // Valor real
+
+  const currencyValueConverted = document.querySelector(".currency-value"); // Outras Moedas
+
   const dolarToday = 5.32;
 
-  const convertedValue = inputCurrencyValue / dolarToday
+  const convertedValue = inputCurrencyValue / dolarToday;
 
-  console.log(convertedValue);
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
+
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(convertedValue);
 }
 
 convertButton.addEventListener("click", convertValues);
