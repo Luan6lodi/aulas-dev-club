70d3c39e99fa83034035e794c7d3fc5fa439a355
luan6lodi <luan6loid@gmail.com>
2026-01-27 19:05:58 -0300

    aula dia 27/01/26

--- JavaScript/0-projeto.js
+++ JavaScript/0-projeto.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".Currency-select");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -9,19 +10,27 @@ function convertValues() {
 
   const currencyValueConverted = document.querySelector(".currency-value"); // Outras Moedas
 
+  console.log(currencySelect.value);
   const dolarToday = 5.32;
+  const euroToday = 6.2;
 
-  const convertedValue = inputCurrencyValue / dolarToday;
+  if (currencySelect.value == "dolar") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+
+  if (currencySelect.value == "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
     style: "currency",
     currency: "BRL",
   }).format(inputCurrencyValue);
-
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertedValue);
 }
-
 convertButton.addEventListener("click", convertValues);
