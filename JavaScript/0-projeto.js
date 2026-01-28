1ae02037645ecea4adb9baa03f36f5919a74ac98
luan6lodi <luan6loid@gmail.com>
2026-01-28 19:12:16 -0300

    aula dia 28/01/26

--- JavaScript/0-projeto.js
+++ JavaScript/0-projeto.js
@@ -33,4 +33,23 @@ function convertValues() {
     currency: "BRL",
   }).format(inputCurrencyValue);
 }
+
+function changeCurrency() {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImage = document.querySelector(".currency-img");
+
+  if (currencySelect.value == "dolar") {
+    currencyName.innerHTML = "Dóllar Americano";
+    currencyImage.src = "../assets/dollar.png"
+  }
+
+  if (currencySelect.value == "euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImage.src = "../assets/euro.png"
+  }
+
+  convertValues()
+}
+
+currencySelect.addEventListener("change", changeCurrency);
 convertButton.addEventListener("click", convertValues);
